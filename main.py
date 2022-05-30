cb1350456d12c49877c3726f926f3afa68f977fb
Emmanuella Ndukwe-Obiukwu <>
2022-05-30 19:59:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator!\nAre you Ready to create a name for your band?\nLETS'S GO!!!")
+birth_city = input("What is the name of the City you grew up in?\n")
+pet_name = input("What is the name of your pet?\n")
+band_name = birth_city + ' ' +  pet_name
+print("You band name could be\n", band_name)
